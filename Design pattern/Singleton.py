a1bdb05703d466639c53c15365f0b233a7115e96
taesang <wjdxotkd1@naver.com>
2021-09-17 14:45:55 +0900

    하나의 객체

--- Design pattern/Singleton.py
+++ Design pattern/Singleton.py
@@ -1,1 +1,2 @@
 #TODO: 이파일은 메모리 수정이 필요
+#https://dev-shin.tistory.com/entry/%EC%8B%B1%EA%B8%80%ED%86%A4-%ED%8C%A8%ED%84%B4Singleton-Pattern-%EA%B0%84%EB%8B%A8%ED%95%98%EA%B2%8C-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0
